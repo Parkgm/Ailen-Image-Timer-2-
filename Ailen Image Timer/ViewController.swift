7aa2cf8953e34e8df907fa2edaf15615b3ed0b92
DIT <d7703_21@d7703-21ui-imac.local>
2018-04-05 15:19:27 +0900

    Ailen image with Timer

--- Ailen Image Timer/ViewController.swift
+++ Ailen Image Timer/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 1
+    var myTimer = Timer()
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var numberLabel: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        numberLabel.text = String(counter)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.doAnimation) , userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func buttonStop(_ sender: UIButton) {
+        myTimer.invalidate()
+    }
+    
+    // Timer에 의해 동적으로 호출되는 함수
+    @objc func doAnimation() {
+        if counter == 5 {
+            counter = 1
+        } else {
+            counter = counter + 1
+        }
+        
+        imageView.image = UIImage(named: "frame\(counter).png")
+        numberLabel.text = String(counter)
+    }
+    
+    
 }
 
